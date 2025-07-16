8e4b2aea95f7744c0f5114829766f57197f5116b
tanzia mourin <tanziamourin68@gmail.com>
2025-07-16 17:38:51 +0600

    url add

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:5000`,
+  baseURL: `https://sports-club-sever.vercel.app`,
 });
 
 const useAxios = () => axiosInstance;
